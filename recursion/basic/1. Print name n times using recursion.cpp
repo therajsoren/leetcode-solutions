a52d3c2971a3c6610db1a682dc151db0837fd370
Raj Soren <68795729+therajsoren@users.noreply.github.com>
2024-09-09 02:25:56 +0000

    first

---
+++ recursion/basic/1. Print name n times using recursion.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+void printName(int i, int n) {
+    // base case
+    if (i > n) return;
+    cout << "Raj Soren" << endl;
+    printName(i + 1, n); 
+
+int main() {
+    int n;
+    cin >> n;
+    printName(1, n); // Start with i = 1
+    return 0;
+}
