da4c1604b6a712f9f937b6708344a93642c0aafe
Raj Soren <68795729+therajsoren@users.noreply.github.com>
2024-12-09 19:39:22 +0000

    added new file

---
+++ Linked-List/Doubly Linked List/insertAtTailOfDLL.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Node{
+    public:
+        int data;
+        Node * next;
+        Node * back;
+    
+    Node(int data1, Node * next1,Node * back1){
+        data = data1;
+        next = next1;
+        back = back1;
+    }
+    public:
+        Node(int data1){
+            data = data1;
+            next = nullptr;
+            back = nullptr;
+        }
+};
+
+Node * convertArr2DLL(vector<int> & arr){
+    Node * head = new Node(arr[0]);
+    Node * prev = head;
+    for(int i = 1; i < arr.size(); i++){
+        Node * temp = new Node(arr[i],nullptr,prev);
+        prev->next = temp;
+        prev = temp;
+    }
+    return head;
+}
+Node * insertAtHead(Node * head,int val){
+    Node * newNode = new Node(val);
+    head->back = newNode;
+    newNode->next = head;
+    head = newNode;
+    return head;
+}
+Node * insertAtTail(Node * head,int val){
+    Node * newNode = new Node(val);
+    Node * temp = head;
+    if(!head->next){
+        return insertAtHead(head);
+    }
+    while(temp->next){
+        temp = temp->next;
+    }
+    temp->next = newNode;
+    newNode->back = temp;
+    newNode->next = NULL;
+    return head;
+}
+void print(Node * head){
+    while(head){
+        cout << head->data << " ";
+        head = head->next;
+    }
+}
+
+int main() {
+    vector<int> arr = {12,5,8,7};
+    Node * head = convertArr2DLL(arr);
+    head = insertAtTail(head,9);
+    print(head);
+}
