acb41702b53a6c2daca466d2d20afd86331977c7
Raj Soren <68795729+therajsoren@users.noreply.github.com>
2024-12-10 16:46:47 +0530

    change code

--- recursion/print name n times using recursion.cpp
+++ recursion/print name n times using recursion.cpp
@@ -2,16 +2,16 @@
 
 using namespace std;
 
-void printName(int i, int n) {
+void printName(int cnt, int n) {
     // base case
-    if (i > n) return; 
+    if (cnt == n) return; 
     cout << "Raj Soren" << endl;
-    printName(i + 1, n); 
+    printName(cnt + 1, n); 
 }
 
 int main() {
     int n;
     cin >> n;
-    printName(1, n); // Start with i = 1
+    printName(0, n); // Start with i = 1
     return 0;
 }
