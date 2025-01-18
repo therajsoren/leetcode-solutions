8e2e34da526c00930c299b75f2847a818a7afeb7
Raj Soren <68795729+therajsoren@users.noreply.github.com>
2025-01-18 20:25:12 +0000

    update code

--- graph/Undirected Graph Cycle.cpp
+++ graph/Undirected Graph Cycle.cpp
@@ -2,17 +2,19 @@
 using namespace std;
 
 // https://www.geeksforgeeks.org/problems/detect-cycle-in-an-undirected-graph/1
-
 class Solution {
   public:
     // Function to detect cycle in an undirected graph.
-    bool isCycleDFS(vector<vector<int>>& adj,int u, vector<bool> &visited, int parent){
+    bool isCycleDFS(vector<vector<int>> & adj, int u, vector<bool> &visited,int parent){
         visited[u] = true;
-        for(int & v: adj[u]){
-            if(v == parent) continue;
+        // explore u
+        for(auto & v : adj[u]){
+            if(v == parent) continue; // skip if v == parent
+            if(isCycleDFS(adj,v,visited,u)){
+                return true;
+            }
+            // agar visited hai to cycle form hu rah to return true;
             if(visited[v]) return true;
-            
-            if(isCycleDFS(adj,v,visited,u)) return true;
         }
         return false;
     }
@@ -20,7 +22,7 @@ class Solution {
         int v = adj.size();
         vector<bool> visited(v,false);
         for(int i = 0; i < v; i++){
-            if(!visited[i] && isCycleDFS(adj,i,visited,-1)){
+            if(!visited[i] && isCycleDFS(adj,i,visited,-1)){  // taking -1 as parent
                 return true;
             }
         }
